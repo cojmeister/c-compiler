c9f7f8ee308a81908a6d6c10feada147fa5aba34
cojmeister <luqas.c@gmail.com>
2024-11-25 22:32:57 +0200

    Run evaluates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
+use crate::ast::ASTNode;
+use crate::scan::scan_file;
+use std::io;
+
 mod scan;
+mod ast;
+mod assembly;
+
+// Example usage
+fn main() -> io::Result<()> {
+    use std::fs::File;
+    use std::io::BufReader;
+
+    let file = File::open("scanner_example.test")?;
+    let mut reader = BufReader::new(file);
+
+    let tokens = scan_file(&mut reader)?;
 
 
-fn main() {
-    println!("Hello, world!");
+    for token in tokens.clone() {
+        match token {
+            Ok(token) => println!("Token: {:?}", token),
+            Err(err) => println!("Error at line {}, column {}: invalid character '{}'",
+                                 err.line, err.column, err.character),
+        }
+    }
+
+    match ASTNode::parse(tokens).unwrap().test_evaluate() {
+        Ok(result) => println!("The evaluated expression gives: {:#?}", result),
+        Err(err) => println!("The expression return error: {:#?}", err),
+    }
+
+
+    Ok(())
 }
